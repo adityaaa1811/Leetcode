0246e4e7865a6dd277fe80e0483e76e6513462c1
Aditya Mishra <mishraanshu1735@gmail.com>
2026-08-23 21:26:06 +0530

    Time: 2 ms (23.84%) | Memory: 9.7 MB (18.89%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> nums;
+        stack<string> strs;
+
+        int num = 0;
+        string current = "";
+
+        for(char ch : s){
+            if(isdigit(ch)){
+                num = num * 10 + (ch - '0');
+            }
+            else if(ch == '['){
+                nums.push(num);
+                strs.push(current);
+
+                num = 0;
+                current = "";
+            }
+            else if(isalpha(ch)){
+                current += ch;
+            }
+            else if(ch == ']'){
+                int repeat = nums.top();
+                nums.pop();
+
+                string previous = strs.top();
+                strs.pop();
+
+                string temp = "";
+
+                for(int i = 0; i < repeat; i++) {
+                    temp += current;
+                }
+
+                current = previous + temp;
+            }
+
+        }
+        return current;
+    }
+};
+
+// digit
+//   ↓
+// build number
+
+// [
+//   ↓
+// SAVE → reset
+
+// letter
+//   ↓
+// add to current
+
+// ]
+//   ↓
+// RESTORE → repeat → combine
